c95b7ebfd69da2ff3a47eb4b3e4a184128ea74a9
Parveen Dudekula <dudekulaparveenatp@gmail.com>
2025-11-28 22:12:57 +0530

    adding client on git

---
+++ vite.config.js
@@ -0,0 +1,51 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      fastRefresh: false,   // ⛔ Disable React Fast Refresh
+    }),
+    tailwindcss()
+  ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  },
+});
+
+
+
+
+/*import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  },
+});
+
+
+
+/*import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  },
+})
+*/
